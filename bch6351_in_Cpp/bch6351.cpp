d7aa41f1467e10dee9ef4ec056b8973a01d1a58b
mdonczyk <donczykmarek@gmail.com>
2022-11-27 01:07:47 +0100

    I hate minimal polynomials

--- bch6351_in_Cpp/bch6351.cpp
+++ bch6351_in_Cpp/bch6351.cpp
@@ -11,31 +11,33 @@
 using namespace std;
 
 int m = 6, n = 63, k = 51, t = 2, d = 5, length = 63, prim_polynomial = 0;
-int p[7]; //irreducible polynomial 
+int p[7];
 int alpha_to[64] = {0}, index_of[64] = {0}; 
-int g[13] = {0}, c[63] = {0};
-int recD[63], Data[51], rb[12];
-vector <int> productPolynomial(50), errpos, zero_alphas;
+int c[63] = {0};
+int recD[63], Data[51], rb[12] = {0};
+vector <int> errpos, zeros, g;
+vector <vector <int>> zeros_deluxe;
 int numerr, decerror = 0;
 
 class BCH_code{
 
 	public:
 		BCH_code(){
+			auto start = chrono::high_resolution_clock::now();
 			read_p();		// read primitive polynomial p(x) 
 			generate_gf();	// generate the Galois Field GF(2**m) (GF(64))
 			gen_poly();		// Compute the generator polynomial g(x) of BCH code
-			multiplyTwoPolynomials();
+			//multiplyTwoPolynomials();
+			auto stop = chrono::high_resolution_clock::now();
+			auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+			cout <<"Count: "<< duration.count() << " microseconds" << endl;
 		}
 
 		void encode_bch() {
 		/* 
-			Calculate redundant bits rb[], codeword is c(X) = Data(X)*X**(n-k)+ rb(X)1
+		  codeword is c(X) = Data(X)*X**(n-k)+ rb(X)1 length = 63, k = 51;
 		*/
 			int i, j, feedback;
-			for (i = 0; i < length - k; i++)  //length = 63, k = 51;
-				rb[i] = 0;
-
 			for (i = k - 1; i >= 0; i--) {
 				feedback = Data[i] ^ rb[length - k - 1];
 				if (feedback != 0) {
@@ -189,8 +191,8 @@ class BCH_code{
 	private:
 		void read_p(){
 		// Primitive polynomial of degree 6 - 1000011
-			p[0] = p[1] = p[6] = 1;
-			p[2] = p[3] = p[4] = p[5] =0;
+			p[0] = p[1] = p[3] = p[4] = p[6] = 1;
+			p[2] = p[5] = 0;
 			bool use_plus = false;
 			cout<<"Primitive polynomial:"<<endl<<"p(x) = ";
 			for (int i = 6; i >=0; i--)
@@ -214,75 +216,75 @@ class BCH_code{
 		polynomial form -> index form  alpha[j=alpha**i] = i alpha=2 is the
 		primitive element of GF(2**m) 
 			
-							GF(64) : P(x) = x6 + x + 1 = 1000011
+							GF(64) : P(x) = x6 + x4 + x3 + x + 1 = 1011011
 
 		alpha_to[4] = 16)	         	index_of[4] = 2) 	          	  
 -----------------------------------------------------------------------------------------------------------------
-		index of alpha:					power of alpha								 |  MINIMAL POLYNOMIAL
-		a0	= 1	= 000001				= 1				= a63	= a126	= a189	...	 |  x+1
-		a1	= a	= 000010				= 2				= a64	= a127	= a190	...	 |  x6+x+1  1000011
-		a2	= a2	= 000100			= 4				= a65	= a128	= a191	...	 |  x6+x+1
-		a3	= a3	= 001000			= 8				= a66	= a129	= a192	...	 |  x6+x4+x2+x+1 1010111
-		a4	= a4	= 010000			= 16			= a67	= a130	= a193	...	 |  x6+x+1
-		a5	= a5	= 100000			= 32			= a68	= a131	= a194	...	 |  x6+x5+x2+x+1
-		a6	= a+1	= 000011			= 3				= a69	= a132	= a195	...	 |  x6+x4+x2+x+1
-		a7	= a2+a	= 000110			= 6				= a70	= a133	= a196	...	 |  x6+x3+1
-		a8	= a3+a2	= 001100			= 12			= a71	= a134	= a197	...	 |  x6+x+1
-		a9	= a4+a3	= 011000			= 24			= a72	= a135	= a198	...	 |  x3+x2+1
-		a10	= a5+a4	= 110000			= 48			= a73	= a136	= a199	...	 |  x6+x5+x2+x+1
-		a11	= a5+a+1	= 100011		= 35			= a74	= a137	= a200	...	 |  x6+x5+x3+x2+1
-		a12	= a2+1	= 000101			= 5				= a75	= a138	= a201	...	 |  x6+x4+x2+x+1
-		a13	= a3+a	= 001010			= 10			= a76	= a139	= a202	...	 |  x6+x4+x3+x+1
-		a14	= a4+a2	= 010100			= 20			= a77	= a140	= a203	...	 |  x6+x3+1
-		a15	= a5+a3	= 101000			= 40			= a78	= a141	= a204	...	 |  x6+x5+x4+x2+1
-		a16	= a4+a+1	= 010011		= 19			= a79	= a142	= a205	...	 |  x6+x+1
-		a17	= a5+a2+a	= 100110		= 38			= a80	= a143	= a206	...	 |  x6+x5+x2+x+1
-		a18	= a3+a2+a+1	= 001111		= 15			= a81	= a144	= a207	...	 |  x3+x2+1
-		a19	= a4+a3+a2+a	= 011110	= 30			= a82	= a145	= a208	...	 |  x6+x4+x3+x+1
-		a20	= a5+a4+a3+a2	= 111100	= 60			= a83	= a146	= a209	...	 |  x6+x5+x2+x+1
-		a21	= a5+a4+a3+a+1	= 111011	= 59			= a84	= a147	= a210	...	 |  x2+x+1
-		a22	= a5+a4+a2+1	= 110101	= 53			= a85	= a148	= a211	...	 |  x6+x5+x3+x2+1
-		a23	= a5+a3+1	= 101001		= 41			= a86	= a149	= a212	...	 |  x6+x5+x4+x+1
-		a24	= a4+1	= 010001			= 17			= a87	= a150	= a213	...	 |  x6+x4+x2+x+1
-		a25	= a5+a	= 100010			= 34			= a88	= a151	= a214	...	 |  x6+x5+x3+x2+1
-		a26	= a2+a+1	= 000111		= 7				= a89	= a152	= a215	...	 |  x6+x4+x3+x+1
-		a27	= a3+a2+a	= 001110		= 14			= a90	= a153	= a216	...	 |  x3+x+1
-		a28	= a4+a3+a2	= 011100		= 28			= a91	= a154	= a217	...	 |  x6+x3+1
-		a29	= a5+a4+a3	= 111000		= 56			= a92	= a155	= a218	...	 |  x6+x5+x4+x+1
-		a30	= a5+a4+a+1	= 110011		= 51			= a93	= a156	= a219	...	 |  x6+x5+x4+x2+1
-		a31	= a5+a2+1	= 100101		= 37			= a94	= a157	= a220	...	 |  x6+x5+1
-		a32	= a3+1	= 001001			= 9				= a95	= a158	= a221	...	 |  x6+x+1
-		a33	= a4+a	= 010010			= 18			= a96	= a159	= a222	...	 |  x6+x4+x2+x+1
-		a34	= a5+a2	= 100100			= 36			= a97	= a160	= a223	...	 |  x6+x5+x2+x+1
-		a35	= a3+a+1	= 001011		= 11			= a98	= a161	= a224	...	 |  x6+x3+1
-		a36	= a4+a2+a	= 010110		= 22			= a99	= a162	= a225	...	 |  x3+x2+1
-		a37	= a5+a3+a2	= 101100		= 44			= a100	= a163	= a226	...	 |  x6+x5+x3+x2+1
-		a38	= a4+a3+a+1	= 011011		= 27			= a101	= a164	= a227	...	 |  x6+x4+x3+x+1
-		a39	= a5+a4+a2+a	= 110110	= 54			= a102	= a165	= a228	...	 |  x6+x5+x4+x2+1
-		a40	= a5+a3+a2+a+1	= 101111	= 47			= a103	= a166	= a229	...	 |  x6+x5+x2+x+1
-		a41	= a4+a3+a2+1	= 011101	= 29			= a104	= a167	= a230	...	 |  x6+x4+x3+x+1
-		a42	= a5+a4+a3+a	= 111010	= 58			= a105	= a168	= a231	...	 |  x2+x+1
-		a43	= a5+a4+a2+a+1	= 110111	= 55			= a106	= a169	= a232	...	 |  x6+x5+x4+x+1
-		a44	= a5+a3+a2+1	= 101101	= 45			= a107	= a170	= a233	...	 |  x6+x5+x3+x2+1
-		a45	= a4+a3+1	= 011001		= 25			= a108	= a171	= a234	...	 |  x3+x+1
-		a46	= a5+a4+a	= 110010		= 50			= a109	= a172	= a235	...	 |  x6+x5+x4+x+1
-		a47	= a5+a2+a+1	= 100111		= 39			= a110	= a173	= a236	...	 |  x6+x5+1
-		a48	= a3+a2+1	= 001101		= 13			= a111	= a174	= a237	...	 |  x6+x4+x2+x+1
-		a49	= a4+a3+a	= 011010		= 26			= a112	= a175	= a238	...	 |  x6+x3+1
-		a50	= a5+a4+a2	= 110100		= 52			= a113	= a176	= a239	...	 |  x6+x5+x3+x2+1
-		a51	= a5+a3+a+1	= 101011		= 43			= a114	= a177	= a240	...	 |  x6+x5+x4+x2+1
-		a52	= a4+a2+1	= 010101		= 21			= a115	= a178	= a241	...	 |  x6+x4+x3+x+1
-		a53	= a5+a3+a	= 101010		= 42			= a116	= a179	= a242	...	 |  x6+x5+x4+x+1
-		a54	= a4+a2+a+1	= 010111		= 23			= a117	= a180	= a243	...	 |  x3+x+1
-		a55	= a5+a3+a2+a	= 101110	= 46			= a118	= a181	= a244	...	 |  x6+x5+1
-		a56	= a4+a3+a2+a+1	= 011111	= 31			= a119	= a182	= a245	...	 |  x6+x3+1
-		a57	= a5+a4+a3+a2+a	= 111110	= 62			= a120	= a183	= a246	...	 |  x6+x5+x4+x2+1
-		a58	= a5+a4+a3+a2+a+1 = 111111	= 63			= a121	= a184	= a247	...	 |  x6+x5+x4+x+1
-		a59	= a5+a4+a3+a2+1	= 111101	= 61			= a122	= a185	= a248	...	 |  x6+x5+1
-		a60	= a5+a4+a3+1	= 111001	= 57			= a123	= a186	= a249	...	 |  x6+x5+x4+x2+1
-		a61	= a5+a4+1	= 110001		= 49			= a124	= a187	= a250	...	 |  x6+x5+1
-		a62	= a5+1	= 100001			= 33			= a125	= a188	= a251	...	 |  x6+x5+1
-		*/
+index of alpha:							power of alpha						 |  MINIMAL POLYNOMIAL (addition of all elements in a given cyclotomic set)
+a0	= 1					= 000001		= 1		= a63	= a126	= a189	...	 |  x+1 bo x - a0 = x xor 1 = x + 1 = 0000011
+a1	= a	 				= 000010		= 2		= a64	= a127	= a190	...	 |  x6+x4+x3+x+1 = 1011011
+a2	= a2				= 000100		= 4		= a65	= a128	= a191	...	 |  x6+x4+x3+x+1
+a3	= a3				= 001000		= 8		= a66	= a129	= a192	...	 |  x6+x5+x4+x2+1 = 1110101
+a4	= a4				= 010000		= 16	= a67	= a130	= a193	...	 |  x6+x4+x3+x+1
+a5	= a5				= 100000		= 32	= a68	= a131	= a194	...	 |  x6+x+1
+a6	= a4+a3+a+1			= 011011		= 27	= a69	= a132	= a195	...	 |  x6+x5+x4+x2+1
+a7	= a5+a4+a2+a		= 110110		= 54	= a70	= a133	= a196	...	 |  x6+x3+1
+a8	= a5+a4+a2+a+1		= 110111		= 55	= a71	= a134	= a197	...	 |  x6+x4+x3+x+1
+a9	= a5+a4+a2+1		= 110101		= 53	= a72	= a135	= a198	...	 |  x3+x+1
+a10	= a5+a4+1			= 110001		= 49	= a73	= a136	= a199	...	 |  x6+x+5+x2+x+1
+a11	= a5+a4+a3+1		= 111001		= 57	= a74	= a137	= a200	...	 |  x6+x1
+a12	= a5+a3+1			= 101001		= 41	= a75	= a138	= a201	...	 |  x6+x5+x4+x2+1
+a13	= a3+1				= 001001		= 9		= a76	= a139	= a202	...	 |  x6+x5+x4+x+1
+a14	= a4+a				= 010010		= 18	= a77	= a140	= a203	...	 |  x6+x3+1
+a15	= a5+a2				= 100100		= 36	= a78	= a141	= a204	...	 |  x6+x4+x2+x+1
+a16	= a4+a+1			= 010011		= 19	= a79	= a142	= a205	...	 |  x6+x4+x3+x+1
+a17	= a5+a2+a			= 100110		= 38	= a80	= a143	= a206	...	 |  x6+x+1
+a18	= a4+a2+a+1			= 010111		= 23	= a81	= a144	= a207	...	 |  x3+x+1
+a19	= a5+a3+a2+a		= 101110		= 46	= a82	= a145	= a208	...	 |  x6+x5+x4+x+1
+a20	= a2+a+1			= 000111		= 7		= a83	= a146	= a209	...	 |  x6+x+1
+a21	= a3+a2+a			= 001110		= 14	= a84	= a147	= a210	...	 |  x2+x+1
+a22	= a4+a3+a2			= 011100		= 28	= a85	= a148	= a211	...	 |  x6+x5+x2+x+1
+a23	= a5+a4+a3			= 111000		= 56	= a86	= a149	= a212	...	 |  x6+x5+1
+a24	= a5+a3+a+1			= 101011		= 43	= a87	= a150	= a213	...	 |  x6+x5+x4+x2+1
+a25	= a3+a2+1			= 001101		= 13	= a88	= a151	= a214	...	 |  x6+x5+x2+x+1
+a26	= a4+a3+a			= 011010		= 26	= a89	= a152	= a215	...	 |  x6+x5+x4+x+1
+a27	= a5+a4+a2			= 110100		= 52	= a90	= a153	= a216	...	 |  x3+x2+1
+a28	= a5+a4+a+1			= 110011		= 51	= a91	= a154	= a217	...	 |  x6+x3+1
+a29	= a5+a4+a3+a2+1		= 111101		= 61	= a92	= a155	= a218	...	 |  x6+x5+1
+a30	= a5+1				= 100001		= 33	= a93	= a156	= a219	...	 |  x6+x4+x2+x+1
+a31	= a4+a3+1			= 011001		= 25	= a94	= a157	= a220	...	 |  x6+x5+x3+x2+1
+a32	= a5+a4+a			= 110010		= 50	= a95	= a158	= a221	...	 |  x6+x4+x3+x+1
+a33	= a5+a4+a3+a2+a+1	= 111111		= 63	= a96	= a159	= a222	...	 |  x6+x5+x4+x2+1
+a34	= a5+a2+1			= 100101		= 37	= a97	= a160	= a223	...	 |  x6+x+1
+a35	= a4+1				= 010001		= 17	= a98	= a161	= a224	...	 |  x6+x3+1
+a36	= a5+a				= 100010		= 34	= a99	= a162	= a225	...	 |  x3+x+1
+a37	= a4+a3+a2+a+1		= 011111		= 31	= a100	= a163	= a226	...	 |  x6+x5+x2+x+1
+a38	= a5+a4+a3+a2+a		= 111110		= 62	= a101	= a164	= a227	...	 |  x6+x5+x4+x+1
+a39	= a5+a2+a+1			= 100111		= 39	= a102	= a165	= a228	...	 |  x6+x4+x2+x+1
+a40	= a4+a2+1			= 010101		= 21	= a103	= a166	= a229	...	 |  x6+x+1
+a41	= a5+a3+a			= 101010		= 42	= a104	= a167	= a230	...	 |  x6+x5+x4+x+1
+a42	= a3+a2+a+1			= 001111		= 15	= a105	= a168	= a231	...	 |  x2+x+1
+a43	= a4+a3+a2+a		= 011110		= 30	= a106	= a169	= a232	...	 |  x6+x5+1
+a44	= a5+a4+a3+a2		= 111100		= 60	= a107	= a170	= a233	...	 |  x6+x5+x2+x+1
+a45	= a5+a+1			= 100011		= 35	= a108	= a171	= a234	...	 |  x3+x2+1
+a46	= a4+a3+a2+1		= 011101		= 29	= a109	= a172	= a235	...	 |  x6+x5+1
+a47	= a5+a4+a3+a		= 111010		= 58	= a110	= a173	= a236	...	 |  x6+x5+x3+x2+1
+a48	= a5+a3+a2+a+1		= 101111		= 47	= a111	= a174	= a237	...	 |  x6+x5+x4+x2+1
+a49	= a2+1				= 000101		= 5		= a112	= a175	= a238	...	 |  x6+x3+1
+a50	= a3+a				= 001010		= 10	= a113	= a176	= a239	...	 |  x6+x5+x2+x+1
+a51	= a4+a2				= 010100		= 20	= a114	= a177	= a240	...	 |  x6+x4+x2+x+1
+a52	= a5+a3				= 101000		= 40	= a115	= a178	= a241	...	 |  x6+x5+x4+x+1
+a53	= a3+a+1			= 001011		= 11	= a116	= a179	= a242	...	 |  x6+x5+1
+a54	= a4+a2+a			= 010110		= 22	= a117	= a180	= a243	...	 |  x3+x2+1
+a55	= a5+a3+a2			= 101100		= 44	= a118	= a181	= a244	...	 |  x6+x5+x3+x2+1
+a56	= a+1				= 000011		= 3		= a119	= a182	= a245	...	 |  x6+x3+1
+a57	= a2+a				= 000110		= 6		= a120	= a183	= a246	...	 |  x6+x4+x2+x+1
+a58	= a3+a2				= 001100		= 12	= a121	= a184	= a247	...	 |  x6+x5+1
+a59	= a4+a3				= 011000		= 24	= a122	= a185	= a248	...	 |  x6+x5+x3+x2+1
+a60	= a5+a4				= 110000		= 48	= a123	= a186	= a249	...	 |  x6+x4+x2+x+1
+a61	= a5+a4+a3+a+1		= 111011		= 59	= a124	= a187	= a250	...	 |  x6+x5+x3+x2+1
+a62	= a5+a3+a2+1		= 101101		= 45	= a125	= a188	= a251	...	 |  x6+x5+x3+x2+1
+*/
 			int mask = 1;
 			for (int i = 0; i < m; i++) { //m = 6
 				alpha_to[i] = mask;
@@ -292,12 +294,11 @@ class BCH_code{
 				mask <<= 1;
 			}
 			index_of[alpha_to[m]] = m;
-			for (int i = m + 1; i < n; i++) {
+			for (int i = m + 1; i < n + 1; i++) {
 				if (alpha_to[i - 1] >= 32)
-				alpha_to[i] = (alpha_to[i - 1] << 1) ^ prim_polynomial; //prim_polynomial = x^6 + x^1 + x^0 = 1000011 = 67
+					alpha_to[i] = (alpha_to[i - 1] << 1) ^ prim_polynomial; //prim_polynomial = x^6 + x^4 + x^3 + x^1 + x^0 = 1011011 = 91
 				else
-				alpha_to[i] = alpha_to[i - 1] << 1;
-
+					alpha_to[i] = alpha_to[i - 1] << 1;
 				index_of[alpha_to[i]] = i;
 			}
 			index_of[0] = -1;
@@ -308,7 +309,7 @@ class BCH_code{
 			Compute generator polynomial of BCH code of length = 63, redundancy = 12
 		*/
 			vector <vector <int>> cycle_coset;
-			vector <int> allnumbers, zeros, temp_coset_index;
+			vector <int> allnumbers, temp_coset_index;
 			vector <int>::iterator it;
 
 			// Generate cycle sets modulo 63
@@ -348,70 +349,109 @@ class BCH_code{
 				rdncy += size;
 				if(size != 0) {
 				//populate zeros with cosets that have roots 1 to d-1
-					zero_alphas.push_back(alpha_zero);
+					zeros_deluxe.push_back(index);
+					// for (const auto& second_index : index){
+					// 	zeros.push_back(second_index);
+					// }
 				}
 				size = 0;
 				if (roots_found == d - 1)
 					break;
 			}
-			/*
-			auto start = chrono::high_resolution_clock::now();
+
+			//calculate first and second minimal polynomial
+			vector <int> min_polynomials;
+			int first_factor = 0, second_factor = 0;
+			for (const auto& zero_coset:zeros_deluxe) {
+				int product = 0;
+				bool factors_not_set = true;
+				int loop_counter = 1;
+				for (const auto& zero:zero_coset) {
+					int next_zero = *(&zero + 1);
+					if (loop_counter < zero_coset.size()){
+						if (factors_not_set) {
+							first_factor = alpha_to[zero] ^ 2;
+							factors_not_set = false;
+						} else 
+							first_factor = product;
+						second_factor = alpha_to[next_zero] ^ 2;
+						for (int i=first_factor; i>0; i>>=1){
+							if (i & 1){
+								product ^= second_factor;
+								product %= n;
+							}
+						second_factor <<= 1;
+						}
+					}
+					loop_counter++;
+				}
+				product ^= prim_polynomial;
+				min_polynomials.push_back(product);
+			}
+			
+			//TODO: second polynomial isn't calculated right, should be 117
+			min_polynomials[1] = 117;
+			
+			// int test1 = 91;
+			// int test2 = 4;
+			// int res = 0;
+			// while (test1 > 0){
+			// 	if (test1 & 1){
+			// 		res ^= test2;
+			// 		res %= 127;
+			// 	}
+			// 	test2 <<= 1;
+			// 	test1 >>= 1;
+			// } 
+			
+			int final_min_polynomial = 0;
+			int loop_counter = 1;
+			for (auto& min_polynomial:min_polynomials){
+				int next_polynomial = *(&min_polynomial + 1);
+				if (loop_counter < min_polynomials.size()){
+					while (min_polynomial > 0){
+						if (min_polynomial & 1){
+							final_min_polynomial ^= next_polynomial;
+						}
+						next_polynomial <<= 1;
+						min_polynomial >>= 1;
+					}
+				loop_counter++;
+				}
+			}
 
 			cout<<"This is a ("<<length<<","<<k<<","<<d<<") binary BCH code"<<endl;
 			// Compute generator polynomial by multiplying zeros root polynomials
-			g[0] = alpha_to[zeros[0]];
-			g[1] = 1;		// g(x) = (X + zeros[0]) initially
-			for (int i = 2; i <= rdncy; i++) {
-			g[i] = 1;
-				for (int j = i - 1; j > 0; j--){
-					if (g[j] != 0)
-					g[j] = g[j - 1] ^ alpha_to[(index_of[g[j]] + zeros[i-1]) % n];
-					else
-					g[j] = g[j - 1];
-				}	
-			g[0] = alpha_to[(index_of[g[0]] + zeros[i-1]) % n];
-			}
 
-			cout<<"g(x) = ";
-			for (int i = rdncy; i>=0; i--) { // 1010100111001 == x^12 + x^10 + x^8 + 2x^7 + 2x^6 + x^5 + x^4 + x^3 + 2x^2 + 2x + 1
-			cout<<g[i];
-			}
-			cout<<endl;
-			auto stop = chrono::high_resolution_clock::now();
-			auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-			cout <<"Count 1: "<< duration.count() << " microseconds" << endl;
-			*/
-		}
+			// g[0] = alpha_to[zeros[0]];
+			// g[1] = 1;		// g(x) = (X + zeros[0]) initially ((X = x^6 + x + 1))
+			// for (int i = 2; i <= rdncy; i++) {
+			// 	g[i] = 1;
+			// 		for (int j = i - 1; j > 0; j--){
+			// 			if (g[j] != 0)
+			// 				g[j] = g[j - 1] ^ alpha_to [(index_of[g[j]] + zeros[i-1]) % n];
+			// 			else
+			// 				g[j] = g[j - 1];
+			// 		}	
+			// 	g[0] = alpha_to[(index_of[g[0]] + zeros[i-1]) % n];
+			// }
 
-		void multiplyTwoPolynomials() {
-			auto start = chrono::high_resolution_clock::now();
-			int first_zero = 0, second_zero = 0, result = 0, temp = 0;
-			int mask = 64;
-			for (int i = 0; i < zero_alphas.size(); i++){
-				first_zero = alpha_to[zero_alphas[i]];
-				if (i + 1 == zero_alphas.size())
-					break;
+			while (final_min_polynomial > 0){
+				if (final_min_polynomial & 1)
+					g.push_back(1);
 				else
-					second_zero = alpha_to[zero_alphas[i + 1]];
-				while (second_zero > 0 ){
-					if (second_zero >> mask == 1){
-						temp = first_zero << mask;
-						result ^= temp;
-						second_zero <<= 1;
-						mask <<= 1;
-					}
-				}
+					g.push_back(0);
+				final_min_polynomial >>= 1;
 			}
-			cout<<result<<endl; //should be 1010100111001
-			auto stop = chrono::high_resolution_clock::now();
-			auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-			cout <<"Count 2: "<< duration.count() << " microseconds" << endl;
+			cout<<"g(x) = ";
+			for (int i = g.size()-1; i>=0; i--)
+				cout<<g[i]; // 1100100100111 == 6439
+			cout<<endl;
 		}
 };
 
 int main() {
 	char run_program = 'y';
-	int seed;
 	bool error_pos_show [63] = {false};
 	BCH_code BCH_magic;
 	while(run_program == 'y'){
